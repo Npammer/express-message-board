91fd1ed7dc26d566b3d584c709d3a1eb3d935c44
npammer <npammer@gmail.com>
2020-10-12 15:45:06 +0200

    done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", (req, res) => {
+  const newMessage = {
+    user: req.body.user,
+    text: req.body.text,
+    added: new Date(),
+  };
+
+  messages.push(newMessage);
+  res.redirect("/"); // View
 });
 
 module.exports = router;
